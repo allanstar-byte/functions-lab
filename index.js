a539c6830fa6a0cdfe49da3f1f0b74c4daf29fea
allanstar-byte <allanmutisya263@email.com>
2022-06-02 05:30:46 +0300

    Funtions

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+
+function whisper(string) {
+    return string.toLowerCase()
+}
+
+function logShout(string) {
+    console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+    console.log(string.toLowerCase())
+}
+
+function sayHiToHeadphonedRoommate(shout){
+    const shout_1 = shout.toUpperCase()
+    return shout_1
+}
